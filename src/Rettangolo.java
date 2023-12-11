1827771956839d986ee3f07aa9cefd64c4e0d48e
Stefano <stegallo1990@gmail.comgit config -lgit config --global user.email stegallo1990@gmail.com>
2023-12-11 14:22:01 +0100

    Esercizio Classi astratte 1

---
+++ src/Rettangolo.java
@@ -0,0 +1,15 @@
+public class Rettangolo extends Forma {
+
+    @Override
+    public void calcolaArea() {
+        System.out.println("base * altezza");
+
+
+    }
+}
+
+
+
+
+
+
---
+++ src/Triangolo.java
@@ -0,0 +1,8 @@
+public class Triangolo extends Forma {
+
+    @Override
+    public void calcolaArea() {
+        System.out.println("base * altezza /2");
+
+    }
+}
